6abe28faf61a899686e70541f0bfbd1a1bb2b256
Mitchell Waldman <mitchswaldman@gmail.com>
2018-04-10 23:02:04 -0700

    use https

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
-export const apiEndpoint = process.env.NODE_ENV === 'production' ? 'http://mitchslist-be.herokuapp.com' : 'http://localhost:8000'
+export const apiEndpoint = process.env.NODE_ENV === 'production' ? 'https://mitchslist-be.herokuapp.com' : 'http://localhost:8000'
 export const TOTAL_CATEGORIES = 3
 export const categoryFilterFormName = 'categoryFilterForm'
